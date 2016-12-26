882d6b791dd3558d3a28744a9cd7a371dcae20f2
Allan Kerr <amk564@mail.usask.ca>
2016-12-25 23:14:13 -0600

    Added basic API stubs

--- src/main/java/com/kerr/nearme/events/EventsAPI.java
+++ src/main/java/com/kerr/nearme/events/EventsAPI.java
@@ -3,6 +3,7 @@ package com.kerr.nearme.events;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.response.UnauthorizedException;
 import com.google.appengine.api.users.User;
 import com.kerr.nearme.FirebaseAuthenticator;
 
@@ -13,21 +14,24 @@ import java.util.List;
         description = "API for handling event creation, fetching and queries.")
 public class EventsAPI {
 
-    @ApiMethod(name = "cities.list", authenticators = {FirebaseAuthenticator.class})
-    public List<String> citiesList(User user) {
-        //EventController controller = new EventController();
-        return null;//controller.fetchEventsByUserID(userID);
+    @ApiMethod(name = "cities.list")
+    public List<String> listCityEvents() {
+        return null;
     }
 
-    @ApiMethod(name = "myevents.list")
-    public List<String> myEventsList(@Named("userID") String userID) {
-        //EventController controller = new EventController();
-        return null;//controller.fetchEventsByUserID(userID);
+    @ApiMethod(name = "myevents.list", authenticators = {FirebaseAuthenticator.class})
+    public List<String> listMyEvents(User user) throws UnauthorizedException {
+        if (user == null) {
+            throw new UnauthorizedException("");
+        }
+        return null;
     }
 
-    @ApiMethod(name = "myevents.insert")
-    public List<String> myEventsInsert(@Named("userID") String userID) {
-        //EventController controller = new EventController();
-        return null;//controller.fetchEventsByUserID(userID);
+    @ApiMethod(name = "myevents.add", authenticators = {FirebaseAuthenticator.class})
+    public List<String> addEvent(User user) throws UnauthorizedException  {
+        if (user == null) {
+            throw new UnauthorizedException("");
+        }
+        return null;
     }
 }
