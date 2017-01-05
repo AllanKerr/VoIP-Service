6d39e20be8d2e7cfa7acc4cec1c5ad6b7145bb83
Allan Kerr <amk564@mail.usask.ca>
2017-01-04 21:03:29 -0600

    Added endpoint for access token generation.

--- src/main/java/com/kerr/nearme/account/AccountAPI.java
+++ src/main/java/com/kerr/nearme/account/AccountAPI.java
@@ -5,6 +5,8 @@ import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.response.UnauthorizedException;
 import com.google.appengine.api.users.User;
 import com.kerr.nearme.APIKeys;
+import com.twilio.jwt.accesstoken.AccessToken;
+import com.twilio.jwt.accesstoken.VoiceGrant;
 import com.twilio.sdk.TwilioRestClient;
 import com.twilio.sdk.TwilioRestException;
 import com.twilio.sdk.resource.factory.SmsFactory;
@@ -21,17 +23,33 @@ public class AccountAPI {
     @ApiMethod(name = "send.sms", path = "send/sms")
     public void sendSmsMessage(User user, Message message) throws UnauthorizedException, TwilioRestException {
         if (user == null) {
-        //    throw new UnauthorizedException("");
+            //    throw new UnauthorizedException("");
         }
         TwilioRestClient client = new TwilioRestClient(APIKeys.TWILIO_ACCOUNT_SID, APIKeys.TWILIO_AUTH_TOKEN);
         Account account = client.getAccount();
 
         SmsFactory factory = account.getSmsFactory();
 
-        HashMap<String, String> content = new HashMap<String,String>();
+        HashMap<String, String> content = new HashMap<String, String>();
         content.put("To", message.getPhoneNumber());
         content.put("From", "+13067007600");
         content.put("Body", message.getBody());
         factory.create(content);
+    }
+
+    @ApiMethod(name = "accessToken", path = "accessToken")
+    public TwilioAccessToken accessToken(User user) throws UnauthorizedException {
+        if (user == null) {
+            //throw new UnauthorizedException("");
+        }
+        VoiceGrant grant = new VoiceGrant();
+        grant.setOutgoingApplicationSid("APf952467cd44bb6c6da4edd7b7a0cced2");
+
+        AccessToken token = new AccessToken.Builder(
+                APIKeys.TWILIO_ACCOUNT_SID,
+                APIKeys.TWILIO_API_SID,
+                APIKeys.TWILIO_API_SECRET
+        ).identity("test").grant(grant).build();
+        return new TwilioAccessToken(token);
     }
 }
