c09d73a1fefc629ed2ceb165f2dc350f3f6269a1
Allan Kerr <amk564@mail.usask.ca>
2017-01-05 22:05:22 -0600

    Added servlet for receiving calls and routing them to a client.

--- src/main/java/com/kerr/nearme/ReceiveCallServlet.java
+++ src/main/java/com/kerr/nearme/ReceiveCallServlet.java
@@ -1,6 +1,9 @@
 package com.kerr.nearme;
 
+import com.twilio.sdk.verbs.Dial;
 import com.twilio.sdk.verbs.TwiMLResponse;
+import com.twilio.sdk.verbs.TwiMLException;
+import com.twilio.sdk.verbs.Client;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +17,17 @@ public class ReceiveCallServlet extends HttpServlet {
 
     @Override
     public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        TwiMLResponse twiml = new TwiMLResponse();
+        Dial dial = new Dial();
+        try {
+            // Hard coded until phone numbers can be associated with users
+            dial.append(new Client("ZYh1BJU0QyWbVw1vDgQshDKelCP2"));
+            twiml.append(dial);
+        } catch (TwiMLException e) {
 
+        }
+        resp.setContentType("application/xml");
+        resp.getWriter().print(twiml.toXML());
     }
 }
 
