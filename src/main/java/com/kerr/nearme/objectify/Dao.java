3bfaaccee3ad7de5a2d1ab7f06b3755695caff59
Allan Kerr <amk564@mail.usask.ca>
2017-01-08 13:49:10 -0600

    Added a generic data access class for entity objects

---
+++ src/main/java/com/kerr/nearme/objectify/Dao.java
@@ -0,0 +1,36 @@
+package com.kerr.nearme.objectify;
+
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.ObjectifyService;
+
+/**
+ * Created by allankerr on 2017-01-08.
+ */
+
+public class Dao<T> {
+
+    private final Class<?> entityClass;
+
+    public <T>Dao(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+
+    public Key<T> save(T object) {
+        return ObjectifyService.ofy().save().entity(object).now();
+    }
+
+    public void deleteEntities(T... objects) {
+        ObjectifyService.ofy().delete().entities(objects).now();
+    }
+    public void deleteEntity(T object) {
+        ObjectifyService.ofy().delete().entity(object).now();
+    }
+
+    public T load(String id){
+        return (T) ObjectifyService.ofy().load().type(entityClass).id(id).now();
+    }
+
+    public Iterable<T> loadAll(){
+        return (Iterable<T>) ObjectifyService.ofy().load().type(entityClass).iterable();
+    }
+}
