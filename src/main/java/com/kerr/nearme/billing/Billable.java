a7b433b68d308ed076d1dc398c20cea85cb2f601
Allan Kerr <personal.allankerr@gmail.com>
2017-01-08 21:20:20 -0600

    Added sms billable for processing sms's that are sent and received

--- src/main/java/com/kerr/nearme/billing/Billable.java
+++ src/main/java/com/kerr/nearme/billing/Billable.java
@@ -1,5 +1,6 @@
 package com.kerr.nearme.billing;
 
+import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
@@ -7,6 +8,7 @@ import com.googlecode.objectify.annotation.Parent;
 import com.googlecode.objectify.condition.IfTrue;
 import com.kerr.nearme.account.Account;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.logging.Logger;
 
@@ -14,47 +16,67 @@ import java.util.logging.Logger;
  * Created by allankerr on 2017-01-08.
  */
 @Entity
-abstract class Billable {
+abstract class Billable implements Serializable {
 
     private static final Logger logger = Logger.getLogger(Billable.class.getName());
     /**
      * Determines whether or not the billable has been applied to the account's credit balance
      */
     @Index(IfTrue.class)
-    protected boolean isPending = true;
+    private boolean isPending = true;
     /**
      * The account that incurred the billable
      */
     @Parent
-    protected Account account;
+    private Ref<Account> accountRef;
+
     /**
      * The date and time the billable was created.
      */
     @Index
-    protected Date creationDate;
+    private Date creationDate;
     /**
      * The date and time the billable was recorded.
      */
     @Index
-    protected Date recordDate;
+    private Date recordDate;
+
     /**
      * The price of the billable to be set when it is recorded.
      */
-    protected Double price;
+    private Double price;
     /**
      * The SID for uniquely identifying the billable
      */
     protected String billableSid;
+
+    protected boolean isPending() {
+        return isPending;
+    }
+
+    protected boolean hasPrice() {
+        return price != null;
+    }
+
+    protected Double getPrice() {
+        return price;
+    }
+
     @Id
     private Long id;
 
     private Billable() {
     }
 
-    protected Billable(Account account, String billableSid) {
-        this.account = account;
+    protected Billable(Ref<Account> account, String billableSid) {
+        this.accountRef = account;
         this.billableSid = billableSid;
         this.creationDate = new Date();
+    }
+
+    protected Billable(Ref<Account> account, String billableSid, Double price) {
+        this(account, billableSid);
+        this.price = price;
     }
 
     /**
@@ -62,26 +84,22 @@ abstract class Billable {
      * Subclasses must determine how much to deduct and set is pending to true once
      * the balance has been accounted for.
      */
-    public abstract void recordTransaction();
+    public abstract void process();
 
     /**
      * Update the transaction to reflect that it has been recorded at a particular price.
      *
      * @param price The price of the transaction.
      */
-    protected void recordingSucceeded(Double price) {
+    protected void finishedProcessing(Double price) {
+
+        Account account = accountRef.get();
+
         this.price = price;
         this.recordDate = new Date();
         this.isPending = false;
 
         // TODO decrement account credits
-
-        new BillableDao().save(this);
-        logger.info("Successfully recorded transaction" + billableSid + " for " + account.getUserId());
-    }
-
-    protected void recordingFailed() {
-        new BillableDao().save(this);
-        logger.info("Failed to record transaction " + billableSid);
+        logger.info("Successfully recorded transaction" + billableSid + " for ");
     }
 }
--- src/main/java/com/kerr/nearme/billing/SmsBillable.java
+++ src/main/java/com/kerr/nearme/billing/SmsBillable.java
@@ -1,5 +1,6 @@
 package com.kerr.nearme.billing;
 
+import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.Subclass;
 import com.kerr.nearme.APIKeys;
 import com.kerr.nearme.account.Account;
@@ -7,37 +8,43 @@ import com.kerr.twilio.messages.Message;
 import com.kerr.twilio.messages.MessageRequest;
 
 import java.io.IOException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Created by allankerr on 2017-01-08.
  */
-@Subclass
+@Subclass(index=true)
 public class SmsBillable extends Billable {
 
     private static final Logger logger = Logger.getLogger(SmsBillable.class.getName());
 
-    public SmsBillable(Account account, String smsSid) {
+    public SmsBillable(Ref<Account> account, String smsSid) {
         super(account, smsSid);
     }
 
+    public SmsBillable(Ref<Account> account, String smsSid, Double price) {
+        super(account, smsSid, price);
+    }
+
     @Override
-    public void recordTransaction() {
+    public void process() {
         logger.fine("Attempting to record transaction");
-        if (!isPending) {
+        if (!isPending()) {
             // Ensure billables are only recorded once.
             logger.warning("Attempted to record transaction that has already recorded.");
             return;
         }
-        try {
-            Message message = new MessageRequest(APIKeys.TWILIO_ACCOUNT_SID, APIKeys.TWILIO_AUTH_TOKEN, billableSid).fetchResponse();
-            Double price = Double.parseDouble(message.getPrice());
-            recordingSucceeded(price);
-        } catch (IOException e) {
-            recordingFailed();
-
-            logger.log(Level.WARNING, e.getMessage(), e);
+        Double price;
+        if (hasPrice()) {
+            price = getPrice();
+        } else {
+            try {
+                Message message = new MessageRequest(APIKeys.TWILIO_ACCOUNT_SID, APIKeys.TWILIO_AUTH_TOKEN, billableSid).fetchResponse();
+                price = Double.parseDouble(message.getPrice());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+        finishedProcessing(price);
     }
 }
