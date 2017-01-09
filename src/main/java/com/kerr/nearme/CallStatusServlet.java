477b97e425c9af588bc20ded23b0cf5f7c18f56e
Allan Kerr <personal.allankerr@gmail.com>
2017-01-08 22:48:20 -0600

    Added a call status servlet to receive a callback when call ends to handle billing.

---
+++ src/main/java/com/kerr/nearme/CallStatusServlet.java
@@ -0,0 +1,20 @@
+package com.kerr.nearme;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by allankerr on 2017-01-08.
+ */
+public class CallStatusServlet extends HttpServlet {
+
+    private static final String CALL_SID_PARAM = "CallSid";
+
+    @Override
+    public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+    }
+
+}
