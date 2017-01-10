06f201fda8491e109e0f57bd17b18b8db8dfa8c2
Allan Kerr <personal.allankerr@gmail.com>
2017-01-09 19:27:06 -0600

    Updated the call servlet to use constants from the call servlet parameters class.

--- src/main/java/com/kerr/nearme/calls/CallServlet.java
+++ src/main/java/com/kerr/nearme/calls/CallServlet.java
@@ -1,7 +1,5 @@
-package com.kerr.nearme;
+package com.kerr.nearme.calls;
 
-import com.kerr.nearme.calls.CallParticipant;
-import com.kerr.nearme.calls.CallParticipantFactory;
 import com.twilio.sdk.verbs.Dial;
 import com.twilio.sdk.verbs.TwiMLException;
 import com.twilio.sdk.verbs.TwiMLResponse;
@@ -10,27 +8,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.logging.Logger;
 
 /**
  * Created by allankerr on 2017-01-02.
  */
-public class CallServlet extends HttpServlet {
-
-    private static final Logger logger = Logger.getLogger(CallServlet.class.getName());
-
+class CallServlet extends HttpServlet {
 
     @Override
     public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-        String to = req.getParameter("To");
-        String from = req.getParameter("From");
+        String to = req.getParameter(CallServletParameters.TO);
+        String from = req.getParameter(CallServletParameters.FROM);
 
         CallParticipant toParticipant = CallParticipantFactory.create(to);
         CallParticipant fromParticipant = CallParticipantFactory.create(from);
-
-        logger.info("From: " + fromParticipant.toString());
-        logger.info("To: " + toParticipant.toString());
 
         TwiMLResponse twiml = new TwiMLResponse();
         Dial dial = new Dial();
---
+++ src/main/java/com/kerr/nearme/calls/CallServletParameters.java
@@ -0,0 +1,9 @@
+package com.kerr.nearme.calls;
+
+/**
+ * Created by allankerr on 2017-01-08.
+ */
+class CallServletParameters {
+    static final String TO = "To";
+    static final String FROM = "From";
+}
