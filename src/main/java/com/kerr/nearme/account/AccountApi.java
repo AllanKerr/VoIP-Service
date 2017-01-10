f88a5998eeb692bb511e4396cc8b82d667bc7f18
Allan Kerr <personal.allankerr@gmail.com>
2017-01-10 00:45:39 -0600

    Moved the SMS sending endpoint to its own API class.

--- src/main/java/com/kerr/nearme/account/AccountApi.java
+++ src/main/java/com/kerr/nearme/account/AccountApi.java
@@ -4,54 +4,22 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.response.UnauthorizedException;
 import com.google.appengine.api.users.User;
-import com.googlecode.objectify.Key;
 import com.googlecode.objectify.ObjectifyService;
 import com.googlecode.objectify.VoidWork;
 import com.kerr.nearme.ApiKeys;
 import com.kerr.nearme.FirebaseAuthenticator;
-import com.kerr.nearme.billing.BillableQueue;
-import com.kerr.nearme.billing.SmsBillable;
 import com.kerr.nearme.objectify.Dao;
 import com.kerr.twilio.incomingphonenumbers.BuyPhoneNumberRequest;
 import com.kerr.twilio.incomingphonenumbers.IncomingPhoneNumber;
 import com.twilio.jwt.accesstoken.AccessToken;
 import com.twilio.jwt.accesstoken.VoiceGrant;
-import com.twilio.sdk.TwilioRestClient;
-import com.twilio.sdk.TwilioRestException;
-import com.twilio.sdk.resource.factory.MessageFactory;
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
 
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
 
 @Api(name = "account", version = "v1",
         title = "Account API",
         description = "API for handling event creation, fetching and queries.")
 public class AccountApi {
-
-
-    @ApiMethod(name = "send.sms", path = "send/sms")
-    public void sendSmsMessage(User user, Message message) throws UnauthorizedException, TwilioRestException {
-        if (user == null) {
-            user = new User("work.allankerr@gmail.com", "", "ZYh1BJU0QyWbVw1vDgQshDKelCP2");
-            //throw new UnauthorizedException("");
-        }
-        TwilioRestClient client = new TwilioRestClient(ApiKeys.TWILIO_ACCOUNT_SID, ApiKeys.TWILIO_AUTH_TOKEN);
-        MessageFactory factory = client.getAccount().getMessageFactory();
-
-        AccountDao accountDao = new AccountDao();
-        Account account = accountDao.load(user.getUserId());
-        List<NameValuePair> content = new LinkedList<NameValuePair>();
-        content.add(new BasicNameValuePair("To", message.getPhoneNumber()));
-        content.add(new BasicNameValuePair("From", account.getOutgoingNumber().getPhoneNumber()));
-        content.add(new BasicNameValuePair("Body", message.getBody()));
-        com.twilio.sdk.resource.instance.Message sent = factory.create(content);
-
-        Key<Account> accountKey = Key.create(Account.class, user.getUserId());
-        BillableQueue.push(new SmsBillable(accountKey, sent.getSid()));
-    }
 
     @ApiMethod(name = "buyPhoneNumber", path = "buyPhoneNumber", authenticators = {FirebaseAuthenticator.class})
     public void buyPhoneNumber(final User user, PhoneNumber number) throws UnauthorizedException, IOException {
